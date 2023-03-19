01250acd3ce3ae737653a11ece2e589648f56a1a
Mahendra Priolkar <mahepriol1313@gmail.com>
2023-03-19 15:58:36 +0530

    improvements

--- MADF_Labs/quicksort.c
+++ MADF_Labs/quicksort.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-int a[100];
+int a[100], n;
 void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+void disp_array(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
 }
 int Partition(int m, int p)
 {
@@ -31,20 +37,22 @@ void quicksort(int p, int q)
 {
     if (p < q)
     {
+        disp_array(a, n);
         int j = Partition(p, q + 1);
+        printf("j=%d\n", j + 1);
         quicksort(p, j);
         quicksort(j + 1, q);
     }
 }
 int main()
 {
-    int n;
     printf("Enter the size of the array.\n");
     scanf("%d", &n);
     printf("Enter the elements.\n");
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
     a[n] = __INT32_MAX__;
+    printf("The quicksort steps are:\n");
     quicksort(0, n);
     printf("The sorted elements are:\n");
     for (int i = 0; i < n; i++)
