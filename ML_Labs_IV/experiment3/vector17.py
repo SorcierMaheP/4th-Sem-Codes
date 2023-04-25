4a34c7697981649e387f85dfa98ccbf2aca2eac3
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2023-04-25 23:25:30 +0530

    hmm

---
+++ ML_Labs_IV/experiment3/vector17.py
@@ -0,0 +1,11 @@
+# not complete yet
+
+import numpy as np
+from numpy.polynomial import Polynomial as P
+
+
+# def polynomial(x, y):
+#     print("sum = ", P.(x, y))
+
+
+#print(polynomial((1, 2, 3), (1, 2, 3)))
