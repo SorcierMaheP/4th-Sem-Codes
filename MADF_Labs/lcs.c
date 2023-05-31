949798cebe66a1233e5aa43470683275aaf2d791
Mahendra Priolkar <mahepriol1313@gmail.com>
2023-05-31 10:54:07 +0530

    added display of subsequence

--- MADF_Labs/lcs.c
+++ MADF_Labs/lcs.c
@@ -41,6 +41,28 @@ int LCS(char s1[], char s2[], int m, int n, int matrix[m + 1][n + 1])
     return matrix[m][n];
 }
 
+void sequence(int ss[], int m, int n, int matrix[m + 1][n + 1])
+{
+    int i = m, j = n, k = m;
+    while (i > 0)
+    {
+        if (matrix[i][j] == matrix[i - 1][j])
+            ss[k] = 0, k--, i--;
+        else
+            ss[k] = 1, i--, j--, k--;
+    }
+}
+
+void print(char str[], int ss[], int m)
+{
+    for (int i = 1, j = 0; i <= m, j < m; i++, j++)
+    {
+        if (ss[i])
+            printf("%c", str[j]);
+    }
+    printf("\n");
+}
+
 int main()
 {
     int matrix[MAX][MAX];
@@ -48,9 +70,13 @@ int main()
     char str2[] = "KLLKNKLLKNYY";
     int n = strlen(str1);
     int m = strlen(str2);
+    int ss[m];
     int k = LCS(str1, str2, m, n, matrix);
     printf("String 1: %s\n", str1);
     printf("String 2: %s\n", str2);
     printf("Answer: %d\n", k);
+    sequence(ss, m, n, matrix);
+    printf("The subsequence is: ");
+    print(str2, ss, m);
     return 0;
 }
